cb7d1493ac5071ce3d1459ee94d54cee54cb7092
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-16 00:14:27 +0300

    feat: formate the home page

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,10 +1,11 @@
-
 function Home() {
   return (
     <>
-        
+      <div className="mx-auto w-full max-w-2xl px-6 py-10">
+        <h1 className="text-center text-2xl font-bold">TODO LIST</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
