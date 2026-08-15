6812a6fa2966248aff443b7d0c861e4d4a46cd13
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-16 00:18:02 +0300

    feat: add SearchBar component

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,8 +1,12 @@
+import SearchBar from "../components/SearchBar.jsx";
+
 function Home() {
   return (
     <>
       <div className="mx-auto w-full max-w-2xl px-6 py-10">
         <h1 className="text-center text-2xl font-bold">TODO LIST</h1>
+
+        <SearchBar />
       </div>
     </>
   );
