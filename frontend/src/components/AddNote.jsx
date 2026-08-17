402af55609b44437e3f0b5d0703b6ac31fc27cc3
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-17 12:03:33 +0300

    style: update layout and spacing in AddNote component and responsivness

--- frontend/src/components/AddNote.jsx
+++ frontend/src/components/AddNote.jsx
@@ -8,16 +8,16 @@ function AddNote({ isOpen, onCancel }) {
   return (
     <>
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-4">
-            <div className="w-full max-w-[30%] bg-white rounded-lg p-6 ">
+            <div className="w-full max-w-md bg-white rounded-lg p-6">
                 <h1 className="text-xl font-black text-center">NEW NOTE</h1>
 
                 <input type="text" placeholder="Input your Note..."
                        className="w-full rounded-sm border border-[#6C63FF] px-4 py-1 mt-5 text-sm outline-none"/>
                 
-                <div className="flex items-center justify-between mt-20">
-                    <button className="bg-[#6C63FF] text-white px-2 py-1 rounded-sm cursor-pointer"
+                <div className="mt-5 flex flex-col-reverse gap-3 sm:mt-10 sm:flex-row sm:justify-between">
+                    <button className="w-full sm:w-auto bg-[#6C63FF] text-white px-2 py-1 rounded-sm cursor-pointer"
                     onClick={onCancel}>Cancel</button>
-                    <button className="bg-[#6C63FF] text-white px-2 py-1 rounded-sm cursor-pointer">Apply</button>
+                    <button className="w-full sm:w-auto bg-[#6C63FF] text-white px-2 py-1 rounded-sm cursor-pointer">Apply</button>
                 </div>
             </div>
         </div>
