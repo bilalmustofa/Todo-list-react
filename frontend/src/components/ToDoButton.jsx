7e4a7150fba2e72fd86804f2cf1840f68eab24c0
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-17 08:34:47 +0300

    style: style the add button

--- frontend/src/components/ToDoButton.jsx
+++ frontend/src/components/ToDoButton.jsx
@@ -1,8 +1,11 @@
+import { FiPlus } from "react-icons/fi";
 
 function ToDoButton() {
   return (
     <>
-        
+        <button className="bg-[#6C63FF] rounded-full p-3 text-white fixed bottom-10 right-[50%] cursor-pointer">
+            <FiPlus size={20}/>
+        </button>
     </>
   )
 }
