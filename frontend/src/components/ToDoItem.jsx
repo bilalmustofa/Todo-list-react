3deea0e39a8c0b5da347bf22172c64727ea4ad1f
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-19 18:00:57 +0300

    feat: add delete functionality for Todo item

--- frontend/src/components/ToDoItem.jsx
+++ frontend/src/components/ToDoItem.jsx
@@ -1,7 +1,7 @@
 import { FiTrash2 } from "react-icons/fi";
 import { VscEdit } from "react-icons/vsc";
 
-function ToDoItem({ title, completed, onToggle }) {
+function ToDoItem({ title, completed, onToggle, onDelete }) {
   return (
     <>
         <div className="flex items-center justify-between border-b border-b-[#6C63FF] py-4 mx-10">
@@ -10,7 +10,7 @@ function ToDoItem({ title, completed, onToggle }) {
                 <p className={`${completed ? "line-through text-gray-400" : ""}`}>{title}</p>
             </div>
             <div className="flex items-center justify-center gap-4 cursor-pointer">
-                <FiTrash2 className="hover:text-[red]"/>
+                <FiTrash2 className="hover:text-[red]" onClick={onDelete}/>
                 <VscEdit className="hover:text-[blue]"/>
             </div>
         </div>
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -6,7 +6,6 @@ import ToDoButton from "../components/ToDoButton";
 import ToDoItem from "../components/ToDoItem";
 
 function Home() {
-
   const [isAddNoteOpen, setAddNoteOpen] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
   const [todos, setTodos] = useState([]);
@@ -16,40 +15,58 @@ function Home() {
     const newTodo = {
       id: Date.now(),
       title: title,
-      completed: false
-    }
+      completed: false,
+    };
     setTodos((prevTodos) => [...prevTodos, newTodo]);
     setAddNoteOpen(false);
-  }
+  };
 
   //  completed todo
   const toggleTodo = (id) => {
-    setTodos((prevTodos) => 
-      prevTodos.map((todo) => 
-        todo.id === id ? { ...todo, completed: !todo.completed} : todo
-      )
-    )
-  }
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+
+  // delete Todo
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
-    <main className={`min-h-screen ${darkMode ? "bg-[#252525] text-white" : "bg-white text-black"}`}>
-      <div className="mx-auto w-full max-w-2xl px-6 py-10">
-        <h1 className="text-center text-2xl font-bold">TODO LIST</h1>
+      <main
+        className={`min-h-screen ${darkMode ? "bg-[#252525] text-white" : "bg-white text-black"}`}
+      >
+        <div className="mx-auto w-full max-w-2xl px-6 py-10">
+          <h1 className="text-center text-2xl font-bold">TODO LIST</h1>
 
-        <Navigation darkMode={darkMode} setDarkMode={setDarkMode}/>
+          <Navigation darkMode={darkMode} setDarkMode={setDarkMode} />
 
-        <div className="mt-8">
-          {todos.map((todo) => (
-            <ToDoItem key={todo.id} title={todo.title} completed={todo.completed} onToggle={() => toggleTodo(todo.id)}/>
-          ))}
+          <div className="mt-8">
+            {todos.map((todo) => (
+              <ToDoItem
+                key={todo.id}
+                title={todo.title}
+                completed={todo.completed}
+                onToggle={() => toggleTodo(todo.id)}
+                onDelete={() => deleteTodo(todo.id)}
+              />
+            ))}
+          </div>
+
+          <ToDoButton onClick={() => setAddNoteOpen(true)} />
+
+          <AddNote
+            isOpen={isAddNoteOpen}
+            onCancel={() => setAddNoteOpen(false)}
+            darkMode={darkMode}
+            onApply={addTodo}
+          />
         </div>
-
-        <ToDoButton onClick={() => setAddNoteOpen(true)}/>
-
-        <AddNote isOpen={isAddNoteOpen} onCancel={() => setAddNoteOpen(false)} darkMode={darkMode} onApply={addTodo}/>
-      </div>
-    </main>
+      </main>
     </>
   );
 }
